e603855f85319edb9ffd295e792c084054728757
Tom Wilson <tom@jackhq.com>
2018-07-06 07:16:07 -0400

    built as es6 module

--- index.js
+++ index.js
@@ -1,42 +1,24 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-
-// const { times, equals, nth, compose,
-//   update, gt, lt, and, not, __, map, addIndex,
-//   sum, add, subtract, tap, flatten
-// } = require('ramda')
-
-var _require = require('fun-fp'),
-    times = _require.times,
-    equals = _require.equals,
-    byIndex = _require.byIndex,
-    compose = _require.compose,
-    update = _require.update,
-    gt = _require.gt,
-    lt = _require.lt,
-    and = _require.and,
-    not = _require.not,
-    map = _require.map,
-    addindex = _require.addindex,
-    sum = _require.sum,
-    add = _require.add,
-    subtract = _require.subtract,
-    flatten = _require.flatten,
-    curry = _require.curry,
-    reduce = _require.reduce;
 
 // get random integer function
 
 
-var randomInt = require('random-int');
-// create a declarative set of functions for finding neighbors
-// const [top, left, bottom, right] = [
-//   subtract(__, 1), subtract(__, 1), add(__, 1), add(__, 1)
-// ]
+var _ramda = require('ramda');
 
-var mapIndex = addindex(map);
-var curryMap = curry(map);
+var _randomInt = require('random-int');
+
+var _randomInt2 = _interopRequireDefault(_randomInt);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var mapIndex = addindex(_ramda.map);
+var curryMap = curry(_ramda.map);
 
 /*
 settings:
@@ -55,8 +37,8 @@ var sim = function sim(_ref, onTick) {
 
   var running = false;
   // create board
-  var board = times(function (n) {
-    return times(createCell, size);
+  var board = (0, _ramda.times)(function (n) {
+    return (0, _ramda.times)(createCell, size);
   }, size);
   // handle game tick
   //   notify new board for redraw
@@ -64,22 +46,22 @@ var sim = function sim(_ref, onTick) {
     // process board
     board = mapIndex(function (v, row) {
       return mapIndex(function (value, col) {
-        var alive = equals(1, value);
+        var alive = (0, _ramda.equals)(1, value);
         var n = neighbors(row, col);
         // 3 neighbors and not alive
-        if (and(equals(n, 3), not(alive))) return 1;
+        if ((0, _ramda.and)((0, _ramda.equals)(n, 3), (0, _ramda.not)(alive))) return 1;
 
         // less than 2 neighbors and alive - decease
-        if (and(lt(n, 2), alive)) return 0;
+        if ((0, _ramda.and)((0, _ramda.lt)(n, 2), alive)) return 0;
 
         // if 2 neighbors and alive then keep alive
-        if (and(equals(n, 2), alive)) return 1;
+        if ((0, _ramda.and)((0, _ramda.equals)(n, 2), alive)) return 1;
 
         // if 3 neighbors and alive then keep alive
-        if (and(equals(n, 3), alive)) return 1;
+        if ((0, _ramda.and)((0, _ramda.equals)(n, 3), alive)) return 1;
 
         // if more than 3 neighbors and alive then decease
-        if (and(gt(n, 3), alive)) return 0;
+        if ((0, _ramda.and)((0, _ramda.gt)(n, 3), alive)) return 0;
 
         // otherwise return existing value
         return value;
@@ -88,7 +70,7 @@ var sim = function sim(_ref, onTick) {
 
     if (onTick) onTick(board);
     console.log(active());
-    if (and(running, active())) {
+    if ((0, _ramda.and)(running, active())) {
       setTimeout(tick, speed);
     }
   }
@@ -110,10 +92,10 @@ var sim = function sim(_ref, onTick) {
   }
 
   function toggle(row, col) {
-    var canToggle = and(not(running), inBoard(size)(row, col));
+    var canToggle = (0, _ramda.and)((0, _ramda.not)(running), inBoard(size)(row, col));
 
     if (canToggle) {
-      var value = equals(1, byIndex(col, byIndex(row, board))) ? 0 : 1;
+      var value = (0, _ramda.equals)(1, byIndex(col, byIndex(row, board))) ? 0 : 1;
       board = updateCell(board, row, col, value);
     }
 
@@ -123,18 +105,18 @@ var sim = function sim(_ref, onTick) {
   }
 
   function createCell() {
-    return generate ? randomInt(0, 1) : 0;
+    return generate ? (0, _randomInt2.default)(0, 1) : 0;
   }
 
   function updateCell(board, row, col, value) {
-    var newRow = compose(update(col, value), byIndex(row))(board);
+    var newRow = (0, _ramda.compose)((0, _ramda.update)(col, value), byIndex(row))(board);
 
-    return update(row, newRow, board);
+    return (0, _ramda.update)(row, newRow, board);
   }
 
   function inBoard(size) {
     return function (row, col) {
-      return and(and(gt(row, -1), gt(col, -1)), and(lt(row, size), lt(col, size)));
+      return (0, _ramda.and)((0, _ramda.and)((0, _ramda.gt)(row, -1), (0, _ramda.gt)(col, -1)), (0, _ramda.and)((0, _ramda.lt)(row, size), (0, _ramda.lt)(col, size)));
     };
   }
 
@@ -147,29 +129,14 @@ var sim = function sim(_ref, onTick) {
   }
 
   function active() {
-    return gt(reduce(function (acc, v) {
-      return acc + sum(v);
+    return (0, _ramda.gt)(reduce(function (acc, v) {
+      return acc + (0, _ramda.sum)(v);
     }, 0, board), 0);
   }
 
   function neighbors(row, col) {
-    return sum(map(getValue, [[subtract(row, 1), subtract(col, 1)], [subtract(row, 1), col], [subtract(row, 1), add(col, 1)], [row, subtract(col, 1)], [row, add(col, 1)], [add(row, 1), subtract(col, 1)], [add(row, 1), col], [add(row, 1), add(col, 1)]]));
+    return (0, _ramda.sum)((0, _ramda.map)(getValue, [[(0, _ramda.subtract)(row, 1), (0, _ramda.subtract)(col, 1)], [(0, _ramda.subtract)(row, 1), col], [(0, _ramda.subtract)(row, 1), (0, _ramda.add)(col, 1)], [row, (0, _ramda.subtract)(col, 1)], [row, (0, _ramda.add)(col, 1)], [(0, _ramda.add)(row, 1), (0, _ramda.subtract)(col, 1)], [(0, _ramda.add)(row, 1), col], [(0, _ramda.add)(row, 1), (0, _ramda.add)(col, 1)]]));
   }
-  // function neighbors (row, col) {
-  //   return compose(
-  //     sum,
-  //     map(getValue)
-  //   )([
-  //     [top(row), left(col)],
-  //     [top(row), col],
-  //     [top(row), right(col)],
-  //     [row, left(col)],
-  //     [row, right(col)],
-  //     [bottom(row), left(col)],
-  //     [bottom(row), col],
-  //     [bottom(row), right(col)]
-  //   ])
-  // }
 };
 
-module.exports = sim;
+exports.default = sim;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-// const { times, equals, nth, compose,
-//   update, gt, lt, and, not, __, map, addIndex,
-//   sum, add, subtract, tap, flatten
-// } = require('ramda')
-
-const { times, equals, byIndex, compose,
-update, gt, lt, and, not, map, addindex,
-sum, add, subtract, flatten, curry, reduce } = require('fun-fp')
+import {
+  times,
+  equals,
+  nth,
+  compose,
+  update,
+  gt,
+  lt,
+  and,
+  not,
+  __,
+  map,
+  addIndex,
+  sum,
+  add,
+  subtract,
+  tap,
+  flatten
+} from 'ramda'
 
 // get random integer function
-const randomInt = require('random-int')
-// create a declarative set of functions for finding neighbors
-// const [top, left, bottom, right] = [
-//   subtract(__, 1), subtract(__, 1), add(__, 1), add(__, 1)
-// ]
-
+import randomInt from 'random-int'
 const mapIndex = addindex(map)
 const curryMap = curry(map)
 
@@ -24,36 +30,37 @@ settings:
 - generate: true | false
 - speed: 1000
 */
-const sim = ({size=20, generate=true, speed=1000}, onTick) => {
+const sim = ({ size = 20, generate = true, speed = 1000 }, onTick) => {
   let running = false
   // create board
   let board = times(n => times(createCell, size), size)
   // handle game tick
   //   notify new board for redraw
-  function tick () {
+  function tick() {
     // process board
     board = mapIndex(
-      (v, row) => mapIndex((value, col) => {
-        const alive = equals(1, value)
-        const n = neighbors(row, col)
-        // 3 neighbors and not alive
-        if (and(equals(n,3), not(alive))) return 1
+      (v, row) =>
+        mapIndex((value, col) => {
+          const alive = equals(1, value)
+          const n = neighbors(row, col)
+          // 3 neighbors and not alive
+          if (and(equals(n, 3), not(alive))) return 1
 
-        // less than 2 neighbors and alive - decease
-        if (and(lt(n,2), alive)) return 0
+          // less than 2 neighbors and alive - decease
+          if (and(lt(n, 2), alive)) return 0
 
-        // if 2 neighbors and alive then keep alive
-        if (and(equals(n,2), alive)) return 1
+          // if 2 neighbors and alive then keep alive
+          if (and(equals(n, 2), alive)) return 1
 
-        // if 3 neighbors and alive then keep alive
-        if (and(equals(n,3), alive)) return 1
+          // if 3 neighbors and alive then keep alive
+          if (and(equals(n, 3), alive)) return 1
 
-        // if more than 3 neighbors and alive then decease
-        if (and(gt(n,3), alive)) return 0
+          // if more than 3 neighbors and alive then decease
+          if (and(gt(n, 3), alive)) return 0
 
-        // otherwise return existing value
-        return value
-      }, v),
+          // otherwise return existing value
+          return value
+        }, v),
       board
     )
 
@@ -71,20 +78,17 @@ const sim = ({size=20, generate=true, speed=1000}, onTick) => {
     tick: tick
   }
 
-  function start () {
+  function start() {
     running = true
     setTimeout(tick, speed)
   }
 
-  function stop () {
+  function stop() {
     running = false
   }
 
-  function toggle (row, col) {
-    const canToggle = and(
-      not(running),
-      inBoard(size)(row, col)
-    )
+  function toggle(row, col) {
+    const canToggle = and(not(running), inBoard(size)(row, col))
 
     if (canToggle) {
       let value = equals(1, byIndex(col, byIndex(row, board))) ? 0 : 1
@@ -96,62 +100,43 @@ const sim = ({size=20, generate=true, speed=1000}, onTick) => {
     return board
   }
 
-  function createCell () {
-    return generate ? randomInt(0,1) : 0
+  function createCell() {
+    return generate ? randomInt(0, 1) : 0
   }
 
   function updateCell(board, row, col, value) {
-    const newRow =  compose(
-      update(col, value),
-      byIndex(row)
-    )(board)
+    const newRow = compose(update(col, value), byIndex(row))(board)
 
     return update(row, newRow, board)
   }
 
   function inBoard(size) {
     return (row, col) =>
-      and(
-        and(gt(row, -1), gt(col, -1)),
-        and(lt(row, size), lt(col, size))
-      )
+      and(and(gt(row, -1), gt(col, -1)), and(lt(row, size), lt(col, size)))
   }
 
-  function getValue ([row, col]) {
-    return inBoard(size)(row,col) ? byIndex(col, byIndex(row, board)) : 0
+  function getValue([row, col]) {
+    return inBoard(size)(row, col) ? byIndex(col, byIndex(row, board)) : 0
   }
 
-  function active () {
-    return gt(reduce((acc,v) => acc + sum(v), 0, board), 0)
+  function active() {
+    return gt(reduce((acc, v) => acc + sum(v), 0, board), 0)
   }
 
   function neighbors(row, col) {
-    return sum(map(getValue, [
-      [subtract(row, 1), subtract(col, 1)],
-      [subtract(row, 1), col],
-      [subtract(row, 1), add(col, 1)],
-      [row, subtract(col, 1)],
-      [row, add(col, 1)],
-      [add(row, 1), subtract(col, 1)],
-      [add(row, 1), col],
-      [add(row, 1), add(col, 1)]
-    ]))
+    return sum(
+      map(getValue, [
+        [subtract(row, 1), subtract(col, 1)],
+        [subtract(row, 1), col],
+        [subtract(row, 1), add(col, 1)],
+        [row, subtract(col, 1)],
+        [row, add(col, 1)],
+        [add(row, 1), subtract(col, 1)],
+        [add(row, 1), col],
+        [add(row, 1), add(col, 1)]
+      ])
+    )
   }
-  // function neighbors (row, col) {
-  //   return compose(
-  //     sum,
-  //     map(getValue)
-  //   )([
-  //     [top(row), left(col)],
-  //     [top(row), col],
-  //     [top(row), right(col)],
-  //     [row, left(col)],
-  //     [row, right(col)],
-  //     [bottom(row), left(col)],
-  //     [bottom(row), col],
-  //     [bottom(row), right(col)]
-  //   ])
-  // }
 }
 
-module.exports = sim
+export default sim
